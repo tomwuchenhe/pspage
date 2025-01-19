390ebdecaf2c3ba1305c5677d68fe12b72e04aa1
Chenhe Wu <chenhewu@chenhes-mbp.home>
2025-01-19 01:38:31 -0500

    prett

--- src/containers/blogs/Blogs.js
+++ src/containers/blogs/Blogs.js
@@ -24,7 +24,7 @@ export default function Blogs() {
   useEffect(() => {
     if (blogSection.displayMediumBlogs === "true") {
       const getProfileData = () => {
-        fetch("/blogs.json")
+        fetch("/pspage/blogs.json")
           .then(result => {
             if (result.ok) {
               return result.json();
